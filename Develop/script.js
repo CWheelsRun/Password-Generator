b1deab289ae3e61447e65534ee45361d3fc0270c
Corey B <corey.bennett@live.com>
2022-07-25 17:52:28 -0400

    developing variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
 
+// create arrays for each type of character
+
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", ",", "<", ".", ">", "?"]
+
+// empty arrays for user input and created password
+
+var inputArray [];
+var outputArray [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
