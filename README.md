9bf1fb9f5e307808e907315a76cb2c249bcf519a
Corey B <corey.bennett@live.com>
2022-07-25 21:53:27 -0400

    testing and updated README

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Password Generator
 ## Website Foundation
-
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+The HTML & CSS did not need modifying, the main goal was to apply proper JavaScript functionality that opened dialogues to generate a password for the user.
+## Proper Criteria
+* Length of 8-128 characters chosen by user
+* At least one of four types of characters:
+    * Uppercase Letters
+    * Lowercase Letters
+    * Numbers
+    * Symbols / Special Characters
+## Testing Criteria
+"If" statements needed to be established that would alert the user if the criteria wasn't met properly.
+## The End Goal
+Once criteria had been met, a password needed to be generated and output to the user that met the criteria they established.
 ## Live website
 [Password Generator](https://cwheelsrun.github.io/Password-Generator/)
--- assets/script.js
+++ assets/script.js
@@ -4,61 +4,63 @@ function generatePassword() {
 
 // create arrays for each type of character
 
-var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", ",", "<", ".", ">", "?"]
+  var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbolArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", ",", "<", ".", ">", "?"];
 
-// empty arrays for user input and created password
+  // empty arrays for user input and created password
 
-var inputArray = [];
-var outputArray = [];
+  var inputArray = [];
+  var outputArray = [];
 
-// call on the user for length of password and character types
+  // call on the user for length of password and character types
 
-var numofCharacters = prompt("How many characters do you want between 8 and 128?")
-var number = confirm("Do you want numbers?")
-var upper = confirm("Do you want uppercase letters?")
-var lower = confirm("Do you want lowercase letters?")
-var symbol = confirm("Do you want special characters or symbols?")
+    var numofCharacters = prompt("How many characters do you want between 8 and 128?");
+    var upper = confirm("Do you want uppercase letters?");
+    var lower = confirm("Do you want lowercase letters?");
+    var number = confirm("Do you want numbers?");
+    var symbol = confirm("Do you want special characters or symbols?");
 
-// test for proper criteria (length between 8 and 128 & at least one character type chosen)
+  // test for proper criteria (length between 8 and 128 & at least one character type chosen)
 
-if (numofCharacters < 8) {
-  alert("Password must have 8 or more characters!")
-}
+  if (numofCharacters < 8) {
+    alert("Password must have 8 or more characters!");
+  }
 
-if (numofCharacters > 128) {
-  alert("Password must have no more than 128 characters!")
-}
+  if (numofCharacters > 128) {
+    alert("Password must have no more than 128 characters!");
+  }
 
-if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialCharacters) {
-  alert('Password must include at least one type of character!')
-}
+  if (!upper && !lower && !number && !symbol) {
+    alert('Password must include at least one type of character!');
+  }
 
-// begin creating the "input array" based on the choices made in the prompt/confirmations
+  // begin creating the "input array" based on the choices made in the prompt/confirmations
 
-if (number){
-  inputArray = inputArray.concat(numberArray)
-}
+  if (upper){
+    inputArray = inputArray.concat(upperArray);
+  }
 
-if (upper){
-  inputArray = inputArray.concat(upperArray)
-}
+  if (lower){
+    inputArray = inputArray.concat(lowerArray);
+  }
 
-if (lower){
-  inputArray = inputArray.concat(lowerArray)
-}
+  if (number){
+    inputArray = inputArray.concat(numberArray);
+  }
 
-if (symbol){
-  inputArray = inputArray.concat(symbolArray)
-}
+  if (symbol){
+    inputArray = inputArray.concat(symbolArray);
+  }
 
-// generate output array based on parameters set by said choices
+  // generate output array based on parameters set by said choices
 
-for (var i = 0; i < numofCharacters; i++) {
-  outputArray.push ((Math.random() * inputArray.length));
-}
+  for (var i = 0; i < numofCharacters; i++) {
+    outputArray.push (inputArray[Math.floor(Math.random() * inputArray.length)]);
+  }
+
+  return outputArray.join("") ;
 
 }
 
